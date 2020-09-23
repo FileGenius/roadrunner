ca0ffc19bb35a2d213c5ef1f1aabae1b08db03bc
appliedanswers <appliedanswers@users.noreply.github.com>
2020-09-23 11:18:28 -0400

    Adding jobs service

--- cmd/rr/main.go
+++ cmd/rr/main.go
@@ -26,6 +26,7 @@ import (
 	rr "github.com/spiral/roadrunner/cmd/rr/cmd"
 
 	// services (plugins)
+	"github.com/spiral/jobs/v2"
 	"github.com/spiral/roadrunner/service/env"
 	"github.com/spiral/roadrunner/service/gzip"
 	"github.com/spiral/roadrunner/service/headers"
@@ -37,7 +38,14 @@ import (
 	"github.com/spiral/roadrunner/service/rpc"
 	"github.com/spiral/roadrunner/service/static"
 
+	// queue brokers
+	"github.com/spiral/jobs/v2/broker/amqp"
+	"github.com/spiral/jobs/v2/broker/beanstalk"
+	"github.com/spiral/jobs/v2/broker/ephemeral"
+	"github.com/spiral/jobs/v2/broker/sqs"
+
 	// additional commands and debug handlers
+	_ "github.com/spiral/jobs/v2/cmd/rr-jobs/jobs"
 	_ "github.com/spiral/roadrunner/cmd/rr/http"
 	_ "github.com/spiral/roadrunner/cmd/rr/limit"
 )
@@ -53,6 +61,14 @@ func main() {
 	rr.Container.Register(health.ID, &health.Service{})
 	rr.Container.Register(gzip.ID, &gzip.Service{})
 	rr.Container.Register(reload.ID, &reload.Service{})
+	rr.Container.Register(jobs.ID, &jobs.Service{
+		Brokers: map[string]jobs.Broker{
+			"amqp":      &amqp.Broker{},
+			"ephemeral": &ephemeral.Broker{},
+			"beanstalk": &beanstalk.Broker{},
+			"sqs":       &sqs.Broker{},
+		},
+	})
 
 	// you can register additional commands using cmd.CLI
 	rr.Execute()
